b5d3ff67782391ca65f812a2ed5a1091294bd560
Dinesh Palanisamy <Dinesh.Palanisamy@Xome.com>
2018-04-15 15:28:25 +0530

    [added comments for backend]

--- twitter-backend/api/controllers/userController.js
+++ twitter-backend/api/controllers/userController.js
@@ -6,52 +6,74 @@ var mongoose = require('mongoose'),
     TweetDetails = mongoose.model('TweetDetails');
 
 /*
-Registers a new user
+  Adds a new tweet
+  required: tweet info and user info
+  output: ''
 */
 exports.add_tweet = function(req, res) {
-      var new_tweet = new Tweet(req.body);
-      new_tweet.save(function(err, user) {
-        if (err)
-          res.send(err);
-        res.status(200).send({});
-      });
-};
-
-exports.get_all_tweet = function(req, res) {
-      console.log(req.body.username)
-      Tweet.find({username : req.body.username}, function(err, tweets) {
-      if (err)
+  var new_tweet = new Tweet(req.body);
+  new_tweet.save(function(err, user) {
+    if (err)
       res.send(err);
-      res.status(200).send(tweets);
-      });
+    res.status(200).send({});
+  });
 };
 
+/*
+  Returns all tweets of the current user
+  required: username
+  output: all tweets for the given user
+*/
+exports.get_all_tweet = function(req, res) {
+  Tweet.find({username : req.body.username}, function(err, tweets) {
+    if (err)
+      res.send(err);
+
+    res.status(200).send(tweets);
+  });
+};
+
+/*
+  Returns single tweet's information such as view, upvote, downvote for the particular id
+  required: tweet id
+  output: all the information of the given tweet
+*/
 exports.get_tweet = function (req,res){
   var result = [];
-
   TweetDetails.findOneAndUpdate({tweet_id: req.params.id}, {$inc: {views:1}}, {new: true, upsert: true}, function(err, doc){
-      if(err){
-          console.log("Something wrong when updating data!");
-      }
-      result.push(doc);
-      Tweet.findById(req.params.id, function(err, tweet) {
+    if(err){
+        console.log("Something wrong when updating data!");
+    }
+    result.push(doc);
+    Tweet.findById(req.params.id, function(err, tweet) {
       if (err)
         res.send(err);
+
       result.push(tweet);
       res.json(result);
     });
   });
 }
 
+/*
+  updates tweet's upvote information
+  required: tweet id
+  output: updated tweet information
+*/
 exports.upvote_tweet = function (req, res) {
   TweetDetails.findOneAndUpdate({tweet_id: req.params.id}, {$inc: {upvote:1}}, {new: true, upsert: true}, function(err, doc){
-      if(err){
-          res.send(err);
-      }
-      res.status(200).send(doc);
+    if(err){
+        res.send(err);
+    }
+    res.status(200).send(doc);
   });
 }
 
+/*
+  updates tweet downvote information
+  required: tweet id
+  output: updated tweet information
+*/
 exports.downvote_tweet = function (req, res) {
   TweetDetails.findOneAndUpdate({tweet_id: req.params.id}, {$inc: {downvote:1}}, {new: true, upsert: true}, function(err, doc){
       if(err){
--- twitter-backend/api/routes/routes.js
+++ twitter-backend/api/routes/routes.js
@@ -1,15 +1,15 @@
 'use strict';
 var jwt = require('jsonwebtoken'),
-expressJwt = require('express-jwt'),
+  expressJwt = require('express-jwt'),
   request = require('request'),
-  passport = require('passport');
-var passportConfig = require('../../config/passport'),
-twitterConfig = require('../../config/twitter.config.js');
+  passport = require('passport'),
+  passportConfig = require('../../config/passport'),
+  twitterConfig = require('../../config/twitter.config.js'),
+  user = require('../controllers/userController');
+
 passportConfig();
 
 module.exports = function(app) {
-	var user = require('../controllers/userController');
-
 
   app.route('/users/add_tweet')
 		.post(user.add_tweet);
@@ -24,118 +24,122 @@ module.exports = function(app) {
 
 
 	app.route('/health-check').get(function(req, res) {
-  res.status(200);
-  res.send('Hello World');
-});
+    res.status(200);
+    res.send('Hello World');
+  });
 
+
+  /*
+    Below functions are used for twitter authentication
+  */
 	var createToken = function(auth) {
-  return jwt.sign({
-    id: auth.id
-  }, 'my-secret',
-  {
-    expiresIn: 60 * 120
-  });
-};
-
-var generateToken = function (req, res, next) {
-  req.token = createToken(req.auth);
-  return next();
-};
-
-var sendToken = function (req, res) {
-  res.setHeader('x-auth-token', req.token);
-  return res.status(200).send(JSON.stringify(req.user));
-};
-
-app.route('/auth/twitter/reverse')
-  .post(function(req, res) {
-    request.post({
-      url: 'https://api.twitter.com/oauth/request_token',
-      oauth: {
-        oauth_callback: "http%3A%2F%2Flocalhost%3A3000%2Ftwitter-callback",
-        consumer_key: twitterConfig.consumerKey,
-        consumer_secret: twitterConfig.consumerSecret
-      }
-    }, function (err, r, body) {
-      if (err) {
-        return res.send(500, { message: e.message });
-      }
-
-      var jsonStr = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
-      res.send(JSON.parse(jsonStr));
+    return jwt.sign({
+      id: auth.id
+    }, 'my-secret',
+    {
+      expiresIn: 60 * 120
     });
-  });
+  };
 
-app.route('/auth/twitter')
-  .post((req, res, next) => {
-    request.post({
-      url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
-      oauth: {
-        consumer_key: twitterConfig.consumerKey,
-        consumer_secret: twitterConfig.consumerSecret,
-        token: req.query.oauth_token
-      },
-      form: { oauth_verifier: req.query.oauth_verifier }
-    }, function (err, r, body) {
-      if (err) {
-        return res.send(500, { message: err.message });
-      }
+  var generateToken = function (req, res, next) {
+    req.token = createToken(req.auth);
+    return next();
+  };
 
-      const bodyString = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
-      const parsedBody = JSON.parse(bodyString);
+  var sendToken = function (req, res) {
+    res.setHeader('x-auth-token', req.token);
+    return res.status(200).send(JSON.stringify(req.user));
+  };
 
-      req.body['oauth_token'] = parsedBody.oauth_token;
-      req.body['oauth_token_secret'] = parsedBody.oauth_token_secret;
-      req.body['user_id'] = parsedBody.user_id;
+  app.route('/auth/twitter/reverse')
+    .post(function(req, res) {
+      request.post({
+        url: 'https://api.twitter.com/oauth/request_token',
+        oauth: {
+          oauth_callback: "http%3A%2F%2Flocalhost%3A3000%2Ftwitter-callback",
+          consumer_key: twitterConfig.consumerKey,
+          consumer_secret: twitterConfig.consumerSecret
+        }
+      }, function (err, r, body) {
+        if (err) {
+          return res.send(500, { message: e.message });
+        }
 
-      next();
+        var jsonStr = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+        res.send(JSON.parse(jsonStr));
+      });
     });
-  }, passport.authenticate('twitter-token', {session: false}), function(req, res, next) {
-      if (!req.user) {
-        return res.send(401, 'User Not Authenticated');
+
+  app.route('/auth/twitter')
+    .post((req, res, next) => {
+      request.post({
+        url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
+        oauth: {
+          consumer_key: twitterConfig.consumerKey,
+          consumer_secret: twitterConfig.consumerSecret,
+          token: req.query.oauth_token
+        },
+        form: { oauth_verifier: req.query.oauth_verifier }
+      }, function (err, r, body) {
+        if (err) {
+          return res.send(500, { message: err.message });
+        }
+
+        const bodyString = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+        const parsedBody = JSON.parse(bodyString);
+
+        req.body['oauth_token'] = parsedBody.oauth_token;
+        req.body['oauth_token_secret'] = parsedBody.oauth_token_secret;
+        req.body['user_id'] = parsedBody.user_id;
+
+        next();
+      });
+    }, passport.authenticate('twitter-token', {session: false}), function(req, res, next) {
+        if (!req.user) {
+          return res.send(401, 'User Not Authenticated');
+        }
+
+        // prepare token for API
+        req.auth = {
+          id: req.user.id
+        };
+
+        return next();
+      }, generateToken, sendToken);
+
+  //token handling middleware
+  var authenticate = expressJwt({
+    secret: 'my-secret',
+    requestProperty: 'auth',
+    getToken: function(req) {
+      if (req.headers['x-auth-token']) {
+        return req.headers['x-auth-token'];
       }
-
-      // prepare token for API
-      req.auth = {
-        id: req.user.id
-      };
-
-      return next();
-    }, generateToken, sendToken);
-
-//token handling middleware
-var authenticate = expressJwt({
-  secret: 'my-secret',
-  requestProperty: 'auth',
-  getToken: function(req) {
-    if (req.headers['x-auth-token']) {
-      return req.headers['x-auth-token'];
-    }
-    return null;
-  }
-});
-
-var getCurrentUser = function(req, res, next) {
-  User.findById(req.auth.id, function(err, user) {
-    if (err) {
-      next(err);
-    } else {
-      req.user = user;
-      next();
+      return null;
     }
   });
-};
 
-var getOne = function (req, res) {
-  var user = req.user.toObject();
+  var getCurrentUser = function(req, res, next) {
+    User.findById(req.auth.id, function(err, user) {
+      if (err) {
+        next(err);
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  };
 
-  delete user['twitterProvider'];
-  delete user['__v'];
+  var getOne = function (req, res) {
+    var user = req.user.toObject();
 
-  res.json(user);
-};
+    delete user['twitterProvider'];
+    delete user['__v'];
 
-app.route('/auth/me')
-  .get(authenticate, getCurrentUser, getOne);
+    res.json(user);
+  };
 
+  app.route('/auth/me')
+    .get(authenticate, getCurrentUser, getOne);
+    
 };
